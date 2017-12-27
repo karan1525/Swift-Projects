6feceeb170e3772d42e25f7c2e23e537b5df337a
Karan Bhargava <karan1525@yahoo.co.in>
2017-12-27 01:50:13 -0800

    Wired up all buttons to play 1 sound

--- Xylophone/Xylophone/ VIewController.swift
+++ Xylophone/Xylophone/ VIewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer?
+    let soundArr = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
     @IBAction func notePressed(_ sender: UIButton) {
         // Each button is tag -> sender.tag
         // Map all buttons to 1 function
+        
+        let selectedSoundFileName: String = soundArr[sender.tag - 1]
+        print(selectedSoundFileName)
         
         playSound()
     }
